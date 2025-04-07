439ae735b7e91612633e81f6449c976d7767d449
Hoang Anh <ngphuchoanganh@gmail.com>
2025-04-07 20:03:30 +0700

    Add node debugger

--- init.lua
+++ init.lua
@@ -470,23 +470,14 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         -- https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
-        --
-
+        tailwindcss = {},
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
@@ -502,9 +493,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'biome',
-        -- 'typescript-language-server',
-        'tailwindcss-language-server',
-        'css-variables-language-server',
+        'node-debug2-adapter',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
